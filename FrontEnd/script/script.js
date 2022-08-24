3bbcbfd4469f0640602a11d354ad28cd4e02eb01
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2022-08-24 20:31:49 -0300

    Atualização do Front-End 24/08/2022

---
+++ FrontEnd/script/script.js
@@ -0,0 +1,16 @@
+
+function close_menu(){
+    if (document.getElementById("navigation_container_open")){
+        document.getElementById("navigation_container_open").style.display = "none";
+        document.getElementById("header_container_id").style.width = "100%";
+        document.getElementById("body_container_id").style.width = "90%";
+        document.getElementById("navigation_container_open").id = "navigation_container_close";
+    }
+
+    else {
+        document.getElementById("navigation_container_close").style.display = "block";
+        document.getElementById("header_container_id").style.width = "80%";
+        document.getElementById("body_container_id").style.width = "78%";
+        document.getElementById("navigation_container_close").id = "navigation_container_open";
+    }
+}
